5f3a66b02c6bc7d1a5e1594af69037e7f68e0ff3
Rimon Dubnov <RimonDubnov@gmail.com>
2020-11-04 08:36:20 -0800

    message

---
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -0,0 +1,27 @@
+package edu.ucsd.cs110.temperature;
+
+import sun.plugin.javascript.navig.FrameArray;
+
+public class Celsius extends Temperature
+{
+    public Celsius(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        return this;
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return new Fahrenheit(((getValue()*9)/5) + 32);
+    }
+
+    public String toString()
+    {
+        // TODO: Complete this method
+        return getValue() + " C";
+    }
+}
